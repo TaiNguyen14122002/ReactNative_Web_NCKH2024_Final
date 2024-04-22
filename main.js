9e41d6b2a00741b067e0fcae935e306e36ea1d4c
TaiNguyen14122002 <2024801030129@student.tdmu.edu.vn>
2024-04-22 20:48:04 +0700

    insert

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
@@ -19,7 +19,7 @@ app.use(
 
 // Database connection
 mongoose
-  .connect(process.env.DB_URI, {
+  .connect("mongodb+srv://TaiNguyen:Tai@cluster0.gm8lsik.mongodb.net/", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
